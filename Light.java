08e98148c394266406e991e2d2f6ca5364baad01
abdullah0314 <abdullah.m0314@gmail.com>
2025-06-30 18:06:41 +0500

    Create Light.java

---
+++ Light.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package smarthomeautomation;
+
+/**
+ *
+ * @author m2975
+ */
+public class Light extends Device 
+{
+    public Light(String id, String name) 
+    {
+        super(id, name);
+    }
+}
+
